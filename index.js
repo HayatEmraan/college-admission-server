d4db9fbe34d9ef374092d7047d3d2bd73818d70c
hayatemraan <emraanhasanhayat@gmail.com>
2023-07-24 21:04:46 +0600

    updated

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 app.use(cors());
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.mongodb_user}:${process.env.mongodb_pass}@cluster0.1ki0ifk.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,10 +23,11 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const collegesData = client.db("CollegeAdmission").collection("Colleges");
     const usersData = client.db("CollegeAdmission").collection("UsersData");
+    const bookCourses = client.db("CollegeAdmission").collection("bookCourses");
 
     app.get("/colleges", async (req, res) => {
       res.send(await collegesData.find({}).toArray());
@@ -35,8 +39,30 @@ async function run() {
       res.send(result);
     });
     app.post("/users", async (req, res) => {
-        const user = req.body;
-      const result = await usersData.insertOne(user);
+      const user = req.body;
+      const find = await usersData.findOne({ email: user.email });
+      console.log(find);
+      if (find) {
+        const result = "User already exists!";
+        return res.send({ result });
+      } else {
+        const result = await usersData.insertOne(user);
+        res.send(result);
+      }
+    });
+    app.post("/bookCourses", async (req, res) => {
+      const data = req.body;
+      const find = await bookCourses.findOne({ email: data.email });
+      if (find) {
+        return res.send({ result: "Course user already exists!" });
+      }
+      const result = await bookCourses.insertOne(data);
+      res.send(result);
+    });
+    app.get("/getBookCourses/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await bookCourses.find(query).toArray();
       res.send(result);
     });
     // Send a ping to confirm a successful connection
